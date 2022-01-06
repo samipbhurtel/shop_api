e3d4bc0cd57cb76efbb131e33f85a52fd05afc1c
samipbhurtel <sambhurtel@gmail.com>
2022-01-06 16:36:53 +0545

    Models Inserted

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successful!"))
+  .catch((err) => console.log("Error"));
+
+app.use(express.json());
+app.use("/api/users/", userRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend Server Is Running! ");
+});
---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: string,
+      require: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: string,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("User", UserSchema);
